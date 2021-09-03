198a9a22f843f93f1f9030ea84f0a98407665578
João Paulo Marquesini <jao@markezine.me>
2021-09-03 00:32:16 -0300

    removes unused imports from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,12 @@
 import 'dart:async';
-import 'dart:io';
 
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as dotEnv;
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:little_light/core/theme/littlelight.theme.dart';
 import 'package:little_light/exceptions/exception_handler.dart';
-import 'package:little_light/screens/initial.screen.dart';
-import 'package:little_light/utils/platform_capabilities.dart';
-import 'package:little_light/widgets/common/queued_network_image.widget.dart';
 
 import 'core/app/littlelight.app.dart';
 
