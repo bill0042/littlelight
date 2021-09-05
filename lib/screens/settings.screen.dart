635d4c69504ba8ec08ad9dc9036a6e01c74e4d4d
João Paulo Marquesini <jao@markezine.me>
2021-09-05 17:38:13 -0300

    moved item_sort_parameter to models folder

--- lib/screens/settings.screen.dart
+++ lib/screens/settings.screen.dart
@@ -9,7 +9,7 @@ import 'package:little_light/models/wish_list.dart';
 import 'package:little_light/screens/add_wishlist.screen.dart';
 import 'package:little_light/services/littlelight/item_notes.service.dart';
 import 'package:little_light/models/character_sort_parameter.dart';
-import 'package:little_light/services/user_settings/item_sort_parameter.dart';
+import 'package:little_light/models/item_sort_parameter.dart';
 import 'package:little_light/services/user_settings/user_settings.service.dart';
 import 'package:little_light/utils/platform_capabilities.dart';
 import 'package:little_light/widgets/common/header.wiget.dart';
